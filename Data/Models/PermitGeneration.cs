4a70514951a61e77321359aecf3f9f4f0a9d6600
D010RES <83914191+d010res@users.noreply.github.com>
2021-07-25 15:46:27 +0430

    gift-permit

---
+++ Data/Models/PermitGeneration.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Data.Models
+{
+    public class PermitGeneration
+    {
+        public int Id { get; set; }
+        public int PermitId { get; set; }
+        public int UserId { get; set; }
+        public int PermitCode { get; set; }
+        public int AmountType { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime CreationDate { get; set; }
+        public User User { get; set; }
+        //TODO:permit model is needed here
+
+    }
+}
