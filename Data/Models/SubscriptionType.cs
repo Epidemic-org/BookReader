27fe98eb943dba1d45a75c0d5b195ad1793ab593
daniyal2224 <61877865+daniyal2224@users.noreply.github.com>
2021-07-25 12:32:56 +0430

    Add SubcriptionType Model

--- Data/Models/SubscriptionType.cs
+++ Data/Models/SubscriptionType.cs
@@ -7,5 +7,15 @@ namespace BookReader.Data.Models
 {
     public class SubscriptionType
     {
+        public int Id { get; set; }
+        public int AdminId { get; set; }
+        public int DayCount { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public decimal PriceAmount { get; set; }
+        public bool IsActive { get; set; }
+        public string Icon { get; set; }
+        public string Pic { get; set; }
     }
 }
