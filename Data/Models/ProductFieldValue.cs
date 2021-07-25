820e8e928d6595800bd6887dd0acf52ae8b5b9c9
daniyal2224 <61877865+daniyal2224@users.noreply.github.com>
2021-07-25 16:05:54 +0430

    add ProductFieldValue Model

---
+++ Data/Models/ProductFieldValue.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Data.Models
+{
+    public class ProductFieldValue
+    {
+        public int ProductId { get; set; }
+        public int FieldId { get; set; }
+        public int FieldValueId { get; set; }
+    }
+}
