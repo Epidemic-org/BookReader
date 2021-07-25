242d3ccfbe16f389e5e8ab6b59fdaff95f7b1041
daniyal2224 <61877865+daniyal2224@users.noreply.github.com>
2021-07-25 16:21:12 +0430

    Add TermType Model

---
+++ Data/Models/TermType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Data.Models
+{
+    public class TermType
+    {
+        public int Id { get; set; }
+        public int AdminId { get; set; }
+        public string Title { get; set; }
+        public decimal TermValue { get; set; }
+        public int AmountType { get; set; }
+        public int TermType1 { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
