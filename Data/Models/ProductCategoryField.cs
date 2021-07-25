9952f16571e47e04f1fe76f8ce152ccf8af96989
daniyal2224 <61877865+daniyal2224@users.noreply.github.com>
2021-07-25 15:53:43 +0430

    Add Product Category Model

---
+++ Data/Models/ProductCategoryField.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Data.Models
+{
+    public class ProductCategoryField
+    {
+        public int Id { get; set; }
+        public int ProductCategoryId { get; set; }
+        public int FieldId { get; set; }
+
+    }
+}
