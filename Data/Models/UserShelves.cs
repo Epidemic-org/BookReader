272c1f7a4c298af70504f4d04811d330d7af57ca
HassanPezeshkiyan <hassanpezeshkiyan@gmail.com>
2021-07-25 16:43:30 +0430

    add user shelves

---
+++ Data/Models/UserShelves.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Data.Models
+{
+    public class UserShelves
+    {
+        public int Id { get; set; }
+        [ForeignKey ("1")]
+        public int UserId { get; set; }
+        [ForeignKey("2")]
+        public int ProductId { get; set; }
+        [ForeignKey("3")]
+        public int ShelfId { get; set; }
+        public DateTime CreationDate { get; set; }
+
+    }
+        
+}
