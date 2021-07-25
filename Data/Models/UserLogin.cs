0783aa6da28ea0999ccc6f9b374e05569d3f7035
D010RES <83914191+d010res@users.noreply.github.com>
2021-07-25 13:51:52 +0430

    userlogin-usertokens

---
+++ Data/Models/UserLogin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Data.Models
+{
+    public class UserLogin
+    {
+        public string LoginProvider { get; set; }
+        public string ProviderKey { get; set; }
+        public string ProviderDisplayName { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Data/Models/UserToken.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Data.Models
+{
+    public class UserToken
+    {
+        public int UserId { get; set; }
+        public string LoginProvider { get; set; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public User User { get; set; }
+    }
+}
