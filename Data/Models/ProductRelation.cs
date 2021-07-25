cb9869cff5b00518211d36b0375c000de542a4fc
daniyal2224 <61877865+daniyal2224@users.noreply.github.com>
2021-07-25 17:36:59 +0430

    add Relation

---
+++ Data/Models/ProductRelation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Data.Models
+{
+    public class ProductRelation
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int RelatedProductId { get; set; }
+        public int RelationType { get; set; }
+    }
+}
