5b78c66384443b54a7f14fbe0d847dc3dea60187
daniyal2224 <61877865+daniyal2224@users.noreply.github.com>
2021-07-25 12:28:21 +0430

    Add SubscriptionTypeItem

---
+++ Data/Models/SubscriptionTypeItems.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Data.Models
+{
+    public class SubscriptionTypeItems
+    {
+        public int Id { get; set; }
+        public int ProductCategoryId { get; set; }
+        public int ProductId { get; set; }
+        public int SubcriptionTypeId { get; set; }
+
+        public ICollection<ProductCategory> ProductCategories { get; set; }
+        public ICollection<Product> Products { get; set; }
+        public ICollection<SubscriptionType> subscriptionTypes  { get; set; }
+    }
+}
