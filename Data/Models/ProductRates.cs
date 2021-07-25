fe7cdb67ee73e51d3b2bb9405ed6e207be184c7b
HassanPezeshkiyan <hassanpezeshkiyan@gmail.com>
2021-07-25 16:39:58 +0430

    add product rates

---
+++ Data/Models/ProductRates.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Data.Models
+{
+    public class ProductRates
+    {
+        public int Id { get; set; }
+        [ForeignKey ("1")]
+        public int UserId { get; set; }
+        [ForeignKey("2")]
+        public int ProductId { get; set; }
+        public double RateValue { get; set; } // real value : c# ??
+        public DateTime  CreationDate { get; set; }
+
+    }
+}
