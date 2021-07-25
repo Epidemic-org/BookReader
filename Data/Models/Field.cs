364325f8c05cd38da41e871ea58c7d30d77fb603
daniyal2224 <61877865+daniyal2224@users.noreply.github.com>
2021-07-25 15:17:29 +0430

    add Field model

---
+++ Data/Models/Field.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Data.Models
+{
+    public class Field
+    {
+        public int Id { get; set; }
+        public int AdminId { get; set; }
+        public int GroupId { get; set; }
+        public string FieldName { get; set; }
+        public string FieldIcon { get; set; }
+        public int FieldType { get; set; }
+        public bool IsRequired { get; set; }
+        public bool IsSearchable { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsGlobal { get; set; }
+        public DateTime CreationDate { get; set; }
+        public ICollection<User> Admins { get; set; }
+        //ToDo : GroupID
+    }
+}
