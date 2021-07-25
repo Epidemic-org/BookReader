8b5ac035e5f184b8a2292ed93da8bde9939d10c8
daniyal2224 <61877865+daniyal2224@users.noreply.github.com>
2021-07-25 17:14:24 +0430

    add SubscriptionInvoicePayment

---
+++ Data/Models/SubscriptionInvoicePayment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Data.Models
+{
+    public class SubscriptionInvoicePayment
+    {
+        public int Id { get; set; }
+        public int SubscriptionInvoiceId { get; set; }
+        public decimal PayAmount { get; set; }
+        public int TransactionId { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
