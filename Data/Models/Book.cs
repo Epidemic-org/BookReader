06a50bf8f607030c1dfee3c88851047a1c4249ee
daniyal2224 <61877865+daniyal2224@users.noreply.github.com>
2021-07-25 09:43:00 +0430

    Complete Book Model

--- Data/Models/Book.cs
+++ Data/Models/Book.cs
@@ -8,12 +8,16 @@ namespace BookReader.Data.Models
     public class Book
     {
         public int Id { get; set; }
+        public int ProductCategoryId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public int CategoryId { get; set; }
-
+        public string Tags { get; set; }
+        public int UserId { get; set; }
+        public int AdminId { get; set; }
+        public bool IsConfirmed { get; set; }
+        public DateTime ConfirmDate { get; set; }
         public DateTime CreationDate { get; set; }
-
+        public DateTime EditionDate { get; set; }
         public Category Category { get; set; }
     }
 }
