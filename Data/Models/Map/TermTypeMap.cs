aa604100e410a0915ba808019eafe43f3a855960
D010RES <83914191+d010res@users.noreply.github.com>
2021-07-26 23:02:51 +0430

    add-TermType2

--- Data/Models/Map/TermTypeMap.cs
+++ Data/Models/Map/TermTypeMap.cs
@@ -14,6 +14,7 @@ namespace BookReader.Data.Models.Map
             builder.HasKey(t => t.Id);
             builder.Property(d => d.Title).HasMaxLength(50).IsRequired(true).IsUnicode(true);           
             builder.Property(t => t.TermValue).IsRequired(true).HasColumnType("decimal").HasPrecision(18, 2);
+            builder.Property(d => d.CreationDate).HasColumnType("datetime2(7)");
             builder.HasOne(t => t.Admin)
                .WithMany(u => u.TermTypes)
                .HasForeignKey(t => t.AdminId)
