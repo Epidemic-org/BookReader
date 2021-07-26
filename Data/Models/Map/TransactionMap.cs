3738899051878d9e82cd7613ac9efcf0609b7327
D010RES <83914191+d010res@users.noreply.github.com>
2021-07-26 23:14:36 +0430

    TransactionMap

---
+++ Data/Models/Map/TransactionMap.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Data.Models.Map
+{
+    public class TransactionMap : IEntityTypeConfiguration<Transaction>
+    {
+        public void Configure(EntityTypeBuilder<Transaction> builder) {
+            builder.HasKey(t => t.Id);
+            builder.Property(d => d.TrackingCode).HasMaxLength(50).IsRequired(true).IsUnicode(true);
+            builder.Property(d => d.BankName).HasMaxLength(50).IsRequired(true).IsUnicode(true);
+            builder.Property(d => d.Description).HasMaxLength(500).IsRequired(true).IsUnicode(true);
+            builder.Property(d => d.CreationDate).HasColumnType("datetime2(7)");
+            builder.Property(t => t.Amount).IsRequired(true).HasColumnType("decimal").HasPrecision(18, 2);
+
+        }
+    }
+}
