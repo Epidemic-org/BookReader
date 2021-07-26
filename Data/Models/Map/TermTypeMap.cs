381c318efecae09884ffef4d6e466fd0662adba6
D010RES <83914191+d010res@users.noreply.github.com>
2021-07-26 23:00:32 +0430

    add-TermType

---
+++ Data/Models/Map/TermTypeMap.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Data.Models.Map
+{
+    public class TermTypeMap : IEntityTypeConfiguration<TermType>
+    {
+        public void Configure(EntityTypeBuilder<TermType> builder) {
+
+            builder.HasKey(t => t.Id);
+            builder.Property(d => d.Title).HasMaxLength(50).IsRequired(true).IsUnicode(true);           
+            builder.Property(t => t.TermValue).IsRequired(true).HasColumnType("decimal").HasPrecision(18, 2);
+            builder.HasOne(t => t.Admin)
+               .WithMany(u => u.TermTypes)
+               .HasForeignKey(t => t.AdminId)
+               .IsRequired(true)
+               .OnDelete(DeleteBehavior.NoAction)
+               ;
+        }
+    }
+}
