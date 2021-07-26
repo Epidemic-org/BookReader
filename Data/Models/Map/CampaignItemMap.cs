6fcb03883e2010ffbf01531d55bbfad7fd91b6c8
daniyal2224 <61877865+daniyal2224@users.noreply.github.com>
2021-07-26 10:05:23 +0430

    CampaingnItemMap Complete

--- Data/Models/Map/CampaignItemMap.cs
+++ Data/Models/Map/CampaignItemMap.cs
@@ -12,6 +12,25 @@ namespace BookReader.Data.Models.Map
         public void Configure(EntityTypeBuilder<CampaignItem> builder)
         {
             builder.HasKey(f => f.Id);
+
+            builder.HasOne(s =>s.Campaingn)
+            .WithMany(g => g.CampaignItems)
+            .HasForeignKey(s => s.CampaingnId)
+            .IsRequired(true)
+            .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(s => s.ProductCategory)
+            .WithMany(g => g.CampaignItems)
+            .HasForeignKey(s => s.ProductCategoryId)
+            .IsRequired(false)
+            .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(s => s.Product)
+            .WithMany(g => g.CampaignItems)
+            .HasForeignKey(s => s.ProductId)
+            .IsRequired(false)
+            .OnDelete(DeleteBehavior.NoAction);
+
         }
     }
 }
