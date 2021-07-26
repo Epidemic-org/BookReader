dfae57df0431aa867e5da0cd8de1d051ce672898
HrYousofian <hr.yousofian@gmail.com>
2021-07-26 20:27:46 +0430

    RolePermission is added

---
+++ Data/Models/Map/RolePermissionMap.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Data.Models.Map
+{
+    public class RolePermissionMap : IEntityTypeConfiguration<RolePermission>
+    {
+        public void Configure(EntityTypeBuilder<RolePermission> builder)
+        {
+            builder.HasKey(q => q.Id);
+            builder.HasOne(q => q.Role)
+                .WithMany(q => q.RolePermissions)
+                .HasForeignKey(q => q.RoleId)
+                .OnDelete(DeleteBehavior.NoAction);
+            builder.HasOne(q => q.FormAction)
+                .WithMany(q => q.RolePermissions)
+                .HasForeignKey(q => q.FormId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
