9ac5e9cf640a385b8d08ff2f358a89c59eacc34e
daniyal2224 <61877865+daniyal2224@users.noreply.github.com>
2021-07-26 20:24:09 +0430

    ScoreLogMap

---
+++ Data/Models/Map/ScoreLogMap.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Data.Models.Map
+{
+    public class ScoreLogMap : IEntityTypeConfiguration<ScoreLog>
+    {
+        public void Configure(EntityTypeBuilder<ScoreLog> builder)
+        {
+            builder.Property(e => e.Id);
+            
+
+            builder.HasOne(d => d.ScoreType)
+                        .WithMany(p => p.ScoreLogs)
+                        .HasForeignKey(d => d.ScoreId).IsRequired()
+                        .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(d => d.User)
+                        .WithMany(p => p.ScoreLogs)
+                        .HasForeignKey(d => d.UserId).IsRequired()
+                        .OnDelete(DeleteBehavior.NoAction);
+            builder.Property(e => e.Description).HasMaxLength(300);
+            builder.Property(e => e.CreationDate).HasColumnType("datetime2").HasMaxLength(7).IsRequired();
+            builder.Property(e => e.ScoreValue).IsRequired();
+
+        }
+    }
+}
