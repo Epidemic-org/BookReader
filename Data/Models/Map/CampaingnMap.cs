2f0f6e73a28cd0fd91f77b45ccf214eb68cc870a
daniyal2224 <61877865+daniyal2224@users.noreply.github.com>
2021-07-26 10:09:47 +0430

    CampaignMap Complete

--- Data/Models/Map/CampaingnMap.cs
+++ Data/Models/Map/CampaingnMap.cs
@@ -13,12 +13,23 @@ namespace BookReader.Data.Models.Map
         {
 
             builder.HasKey(f => f.Id);
+
             builder.Property(d => d.Title).IsRequired(true).HasMaxLength(75).IsUnicode(true);
+
             builder.Property(d => d.StartDate).HasColumnType("datetime2").HasMaxLength(7).IsRequired(false);
+
             builder.Property(d => d.EndDate).HasColumnType("datetime2").HasMaxLength(7).IsRequired(false);
+
             builder.Property(d => d.CreationDate).HasColumnType("datetime2").HasMaxLength(7).IsRequired(true);
-            //adminid foreignkey 
+
+            builder.HasOne(s => s.Admin)
+            .WithMany(g => g.Campaingns)
+            .HasForeignKey(s => s.AdminId)
+            .IsRequired(true)
+            .OnDelete(DeleteBehavior.NoAction);
+
             builder.Property(d => d.AmountType).IsRequired(true).HasColumnType("tinyint");
+
             builder.Property(d => d.AmountValue).IsRequired(true).HasColumnType("decimal").HasPrecision(18, 2);
             builder.Property(d => d.IsActive).IsRequired(true).HasColumnType("bit");
 
