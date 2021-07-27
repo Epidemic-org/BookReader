1224b882d1e6fef5845ab3473075a2e43b6f02c1
daniyal2224 <61877865+daniyal2224@users.noreply.github.com>
2021-07-27 18:11:32 +0430

    OrderItemMap

---
+++ Data/Models/Map/OrderItemMap.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Data.Models.Map
+{
+    public class OrderItemMap : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            
+            builder.Property(e => e.Quantity).HasPrecision(18,2);
+
+            builder.HasOne(d => d.Order)
+                .WithMany(p => p.OrderItems)
+                .HasForeignKey(d => d.OrderId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.HasOne(d => d.Product)
+                .WithMany(p => p.OrderItems)
+                .HasForeignKey(d => d.ProductId)
+                .OnDelete(DeleteBehavior.NoAction);
+        }
+    }
+}
