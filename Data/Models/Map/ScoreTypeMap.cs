854183544248b79a19aaf3166a2c9b6183108d06
daniyal2224 <61877865+daniyal2224@users.noreply.github.com>
2021-07-27 07:03:15 +0430

    ScoreTypeMap

---
+++ Data/Models/Map/ScoreTypeMap.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BookReader.Data.Models.Map
+{
+    public class ScoreTypeMap : IEntityTypeConfiguration<ScoreType>
+    {
+        public void Configure(EntityTypeBuilder<ScoreType> builder)
+        {
+            builder.HasKey(d => d.Id);
+
+            builder.HasOne(d => d.Admin)
+                .WithMany(s => s.ScoreTypes)
+                .HasForeignKey(d => d.AdminId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+
+            builder.Property(d => d.Title).IsRequired().IsUnicode();
+            builder.Property(d => d.ScoreValue).IsRequired().IsUnicode();
+            builder.Property(d => d.IsActive).IsRequired().IsUnicode().HasColumnType("bit");
+            builder.Property(d => d.ActionType).IsRequired().HasColumnType("tinyint");
+            builder.Property(d => d.CreationDate).IsRequired().IsUnicode().HasColumnType("datetime2").HasMaxLength(7);
+            builder.Property(d => d.MinAmount).IsUnicode().HasPrecision(18,0);
+            builder.Property(d => d.StartDate).IsUnicode().HasColumnType("datetime2").HasMaxLength(7);
+            builder.Property(d => d.EndDate).IsUnicode().HasColumnType("datetime2").HasMaxLength(7);
+        }
+    }
+}
