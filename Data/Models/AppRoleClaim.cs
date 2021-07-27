25ddda1d9d626804c9f1ca6be8692c41908ed1cb
D010RES <83914191+d010res@users.noreply.github.com>
2021-07-27 18:53:32 +0430

    add-role-claim

--- Data/Models/AppRoleClaim.cs
+++ Data/Models/AppRoleClaim.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BookReader.Data.Models
 {
-    public class AppRoleClaim
+    public class AppRoleClaim : IdentityRoleClaim<int>
     {
-        public int Id { get; set; }
         public int RoleId { get; set; }
         public string ClaimType { get; set; }
         public string ClaimValue { get; set; }
--- Data/Models/AppUserClaim.cs
+++ Data/Models/AppUserClaim.cs
@@ -8,7 +8,6 @@ namespace BookReader.Data.Models
 {
     public class AppUserClaim: IdentityUserClaim<int>
     {
-        public int Id { get; set; }
         public int UserId { get; set; }
         public string ClaimType { get; set; }
         public string ClaimValue { get; set; }
