af24f737fb4e9d6edd81d403fd4b933d9ae780d5
daniyal2224 <61877865+daniyal2224@users.noreply.github.com>
2021-07-28 18:50:12 +0430

    orderVm

---
+++ ViewModels/OrderVm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.ViewModels
+{
+    public class OrderVm
+    {
+        [Display(Name = "شناسه")]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        [Display(Name= "نام و نام خانوادگی")]
+        public string UserFullName { get; set; }
+        [Display(Name = "تاریخ ثبت سفارش")]
+
+        public DateTime CreationDate { get; set; }
+        public string NCreationDate
+        {
+            get
+            {
+                return CreationDate.ToPersianDateTimeString();
+            }
+
+            set
+            {
+                CreationDate = value.ToEnglishDateTime();
+            }
+        }
+        [Display(Name ="آدرس")]
+        [StringLength(maximumLength: 50)]
+        public string Address { get; set; }
+    }
+}
