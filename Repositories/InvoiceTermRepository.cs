fa37b8c9952092996e43487f305292a14135df17
HassanPezeshkiyan <hassanpezeshkiyan@gmail.com>
2021-07-28 16:34:40 +0430

    Add InvoiceTerm

---
+++ Repositories/InvoiceTermRepository.cs
@@ -0,0 +1,20 @@
+﻿using BookReader.Data;
+using BookReader.Data.Models;
+using BookReader.Repositories.Base;
+using BookReader.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Repositories
+{
+    public class InvoiceTermRepository:BaseRepository<InvoiceTerm>, IInvoiceTermRepository
+    {
+        private readonly ApplicationDbContext _db;
+    public InvoiceTermRepository(ApplicationDbContext db) : base(db)
+    {
+        _db = db;
+    }
+    
+}
