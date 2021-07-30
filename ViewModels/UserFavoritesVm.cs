d5ad4eaecdef08f12d978c012bfe64add36cb92e
HassanPezeshkiyan <hassanpezeshkiyan@gmail.com>
2021-07-30 19:00:23 +0430

    add userfavoritesvm

---
+++ ViewModels/UserFavoritesVm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.ViewModels
+{
+    public class UserFavoritesVm
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+
+        public int ProductId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string NCreationDate
+        {
+            get
+            {
+                return CreationDate.ToPersianDateTimeString();
+            }
+
+            set
+            {
+                CreationDate = value.ToEnglishDateTime();
+            }
+        }
+    }
+}
