941a6a8a39ddd75e9895cfb5d73b48d4a9e914e0
D010RES <83914191+d010res@users.noreply.github.com>
2021-07-30 17:28:05 +0430

    finish-repositories

---
+++ Repositories/UserLogRepository.cs
@@ -0,0 +1,20 @@
+﻿using BookReader.Data;
+using BookReader.Data.Models;
+using BookReader.Repositories.Base;
+using BookReader.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Repositories
+{
+    public class UserLogRepository : BaseRepository<UserLogs>, IUserLogRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public UserLogRepository(ApplicationDbContext db) : base(db) {
+            _db = db;
+        }
+
+    }
+}
---
+++ Repositories/WalletLogRepository.cs
@@ -0,0 +1,19 @@
+﻿using BookReader.Data;
+using BookReader.Data.Models;
+using BookReader.Repositories.Base;
+using BookReader.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.Repositories
+{
+    public class WalletLogRepository : BaseRepository<WalletLog>, IWalletLogRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public WalletLogRepository(ApplicationDbContext db) : base(db) {
+            _db = db;
+        }
+    }
+}
