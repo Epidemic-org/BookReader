7d2efa400232d99ea6a2467d5339af9cb254bf28
HassanPezeshkiyan <hassanpezeshkiyan@gmail.com>
2021-07-30 19:02:05 +0430

    add invoicepaymentvm

---
+++ ViewModels/InvoicePaymentVm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.ViewModels
+{
+    public class InvoicePaymentVm
+    {
+        public int Id { get; set; }
+        public int InvoiceId { get; set; }
+        public decimal PayAmount { get; set; }
+        public int TransactionId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string NCreationDate
+        {
+            get
+            {
+                return CreationDate.ToPersianDateTimeString();
+            }
+
+            set
+            {
+                CreationDate = value.ToEnglishDateTime();
+            }
+        }
+    }
+}
