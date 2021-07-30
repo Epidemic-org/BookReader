6a7f982fcef024f4a4022c6815e50b7ff340cf05
HassanPezeshkiyan <hassanpezeshkiyan@gmail.com>
2021-07-30 19:01:31 +0430

    add productdownloadvm

---
+++ ViewModels/ProductDownloadVm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.ViewModels
+{
+    public class ProductDownloadVm
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ProductId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string NCreationDate
+        {
+            get
+            {
+                return CreationDate.ToPersianDateTimeString();
+            }
+
+            set
+            {
+                CreationDate = value.ToEnglishDateTime();
+            }
+        }
+    }
+}
