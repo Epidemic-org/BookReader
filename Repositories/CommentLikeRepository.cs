033e2a07046d126ebac61901d05a1778d150b83b
D010RES <83914191+d010res@users.noreply.github.com>
2021-07-30 17:20:16 +0430

    fix-comment-like-repos

--- Repositories/CommentLikeRepository.cs
+++ Repositories/CommentLikeRepository.cs
@@ -1,9 +1,7 @@
-<<<<<<< HEAD
-﻿using BookReader.Context;
+
+using BookReader.Context;
 using BookReader.Data;
-=======
-﻿using BookReader.Data;
->>>>>>> f9083c5a95dd3fbc71b6ce5de69993c2eaa29a9b
+using BookReader.Data;
 using BookReader.Data.Models;
 using BookReader.Repositories.Base;
 using BookReader.Repositories.Interfaces;
@@ -14,18 +12,11 @@ using System.Threading.Tasks;
 
 namespace BookReader.Repositories
 {
-<<<<<<< HEAD
+
     public class CommentLikeRepository : BaseRepository<CommentLike>, ICommentLikeRepository
     {
         private readonly ApplicationDbContext _db;
         public CommentLikeRepository(ApplicationDbContext db) : base(db) {
-=======
-    public class CommentLikeRepository : BaseRepository<CommentLike> , ICommentLikeRepository
-    {
-        private readonly ApplicationDbContext _db;
-        public CommentLikeRepository(ApplicationDbContext db) : base(db)
-        {
->>>>>>> f9083c5a95dd3fbc71b6ce5de69993c2eaa29a9b
             _db = db;
         }
     }
