aa2efc47f67f82773dc6b8a4dc71bba88cdd19f7
HassanPezeshkiyan <hassanpezeshkiyan@gmail.com>
2021-07-30 19:00:09 +0430

    add userlogvm

---
+++ ViewModels/UserLogsVm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.ViewModels
+{
+    public class UserLogsVm
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string NCreationDate
+        {
+            get
+            {
+                return CreationDate.ToPersianDateTimeString();
+            }
+
+            set
+            {
+                CreationDate = value.ToEnglishDateTime();
+            }
+        }
+        public string UserIp { get; set; }
+        public string UserDevice { get; set; }
+        public int IsLogin { get; set; }
+
+    }
+}
