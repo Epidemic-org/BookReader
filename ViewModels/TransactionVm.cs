cbf4805fc7ba6b980503a8ec35f1d26dbd095c73
HassanPezeshkiyan <hassanpezeshkiyan@gmail.com>
2021-07-30 19:00:37 +0430

    add transactionvm

---
+++ ViewModels/TransactionVm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.ViewModels
+{
+    public class TransactionVm
+    {
+        public int Id { get; set; }
+        public string BankName { get; set; }
+        public string TrackingCode { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string NCreationDate
+        {
+            get
+            {
+                return CreationDate.ToPersianDateTimeString();
+            }
+
+            set
+            {
+                CreationDate = value.ToEnglishDateTime();
+            }
+        }
+        public bool IsSuccess { get; set; }
+        public string Description { get; set; }
+    }
+}
