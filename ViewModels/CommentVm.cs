934d8bdf9c4dbfec55b14c9ac4d2133168429974
HassanPezeshkiyan <hassanpezeshkiyan@gmail.com>
2021-07-30 18:23:14 +0430

    comment viewmodel added

---
+++ ViewModels/CommentVm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.ViewModels
+{
+    public class CommentVm
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int UserId { get; set; }
+        public string Text { get; set; }
+        public DateTime CreationDate { get; set; }
+       
+    }
+}
