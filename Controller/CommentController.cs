78e1349dc782be7cd062aa3fc0129adb1b615b90
daniyal2224 <61877865+daniyal2224@users.noreply.github.com>
2021-07-31 20:53:24 +0430

    Comment Controller

--- Controller/CommentController.cs
+++ Controller/CommentController.cs
@@ -22,7 +22,12 @@ namespace BookReader.Controller
         public CommentController(IUnitOfWork db) {
             _db = db;
         }
-
+        /// <summary>
+        /// Return All Comments of Page "page" which has "pageSize" comments
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="pageSize"></param>
+        /// <returns>IActionResults of Ok with Comments Model</returns>
         [HttpGet]
         public async Task<IActionResult> GetAll(int page = 1, int pageSize = 10) {
             var commentList = await _db.Comments.GetAll().
@@ -39,7 +44,11 @@ namespace BookReader.Controller
                 ToListAsync();
             return Ok(commentList);
         }
-
+        /// <summary>
+        /// Find Comment by its Id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>Special Comment</returns>
         [HttpGet]
         public async Task<IActionResult> FindById(int id) {
             if (await _db.Comments.IsExists(id)) {
@@ -59,28 +68,55 @@ namespace BookReader.Controller
                 return NotFound();
             }
         }
-
+        /// <summary>
+        /// Add New Comment
+        /// </summary>
+        /// <param name="comment"></param>
+        /// <returns>Comment which is ResultObjectVm</returns>
         [HttpPost]
         public async Task<IActionResult> Post([FromBody] Comment comment) {
             if (!ModelState.IsValid) {
                 return BadRequest(ModelState);
             }
+            comment.IsActive = true;
+            comment.CreationDate = DateTime.Now;
+            comment.UserId = User.GetUserId();
             var result = await _db.Comments.CreateAsync(comment);
+            result.Id = comment.Id;
+            result.Extra = comment;
             return Ok(result);
         }
-
+        /// <summary>
+        /// Edit a Comment
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="comment"></param>
+        /// <returns>Return New ResultObjectVm</returns>
         [HttpPut]
         public async Task<IActionResult> Put(int id, [FromBody] Comment comment) {
             if (!ModelState.IsValid) {
                 return BadRequest(ModelState);
             }
             var result = await _db.Comments.EditAsync(comment);
+            result.Id = comment.Id;
+            result.Extra = comment;
             return Ok(result);
         }
-
+        /// <summary>
+        /// Delete a Comment
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns>ResultViewModel</returns>
         [HttpDelete]
         public async Task<IActionResult> Delete(int id) {
+            var comment =await _db.Comments.Find(id);
+            if(comment == null)
+            {
+                return NotFound();
+            }
             var result = await _db.Comments.DeleteAsync(id);
+            result.Id = comment.Id;
+            result.Extra = comment;
             return Ok(result);
         }
     }
