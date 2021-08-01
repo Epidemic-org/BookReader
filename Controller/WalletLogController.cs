287aa7887f4d2477f548e2518218118b2945118d
HassanPezeshkiyan <hassanpezeshkiyan@gmail.com>
2021-08-01 19:00:36 +0430

    add authorize in order and walletlog controller

--- Controller/WalletLogController.cs
+++ Controller/WalletLogController.cs
@@ -2,6 +2,7 @@
 using BookReader.Data.Models;
 using BookReader.Utillities;
 using BookReader.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +15,7 @@ namespace BookReader.Controller
 {
     [Route("api/[controller]/[action]/{id?}")]
     [ApiController]
+    [Authorize]
     public class WalletLogController : ControllerBase
     {
         private readonly IUnitOfWork _db;
