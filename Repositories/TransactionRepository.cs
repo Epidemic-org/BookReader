aecdc3e8ab7ddec8ceb4bcdb39092b6a06645c25
Abbas Kashi <AbbasKashi@hotmail.com>
2021-08-03 12:57:47 +0430

    jwt ok

--- Repositories/TransactionRepository.cs
+++ Repositories/TransactionRepository.cs
@@ -1,11 +1,12 @@
 ﻿using BookReader.Data;
+using BookReader.Data.Models;
 using BookReader.Repositories.Base;
 using BookReader.Repositories.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Transactions;
+
 
 namespace BookReader.Repositories
 {
