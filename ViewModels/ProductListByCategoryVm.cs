89e3dc0f686dc42713b0448dc8e5a3056b29f2e5
HassanPezeshkiyan <hassanpezeshkiyan@gmail.com>
2021-08-07 14:30:55 +0430

    add productlistbycategory view model

---
+++ ViewModels/ProductListByCategoryVm.cs
@@ -0,0 +1,22 @@
+﻿using BookReader.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookReader.ViewModels
+{
+    public class ProductListByCategoryVm
+    {
+        [Display(Name = "عنوان")]
+
+        [Required(ErrorMessage = "این فیلد {0} اجباری است.")]
+        public string Title { get; set;}
+        public ICollection<ProductAuthor> ProductAuthors { get; set; }
+        public string AuthorFullName { get; set; }
+        public ICollection<ProductPrice> ProductPrices { get; set; }
+
+        public ICollection<ProductRate> ProductRates { get; set; }
+    }
+}
