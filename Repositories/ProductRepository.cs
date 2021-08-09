b993bbbf486638d6555728ecf1d98b31e2b3c9f5
D010RES <83914191+d010res@users.noreply.github.com>
2021-08-09 12:12:08 +0430

    asdsada

--- Repositories/ProductRepository.cs
+++ Repositories/ProductRepository.cs
@@ -14,7 +14,9 @@ using System.Threading.Tasks;
 namespace BookReader.Repositories
 {
     public class ProductRepository : BaseRepository<Product>, IProductRepository {
+
         private readonly ApplicationDbContext _db;
+
         public ProductRepository(ApplicationDbContext db) : base(db) {
             _db = db;
         }
