6564e678fb62778bf6928de8791615c36187ba9e
HassanPezeshkiyan <hassanpezeshkiyan@gmail.com>
2021-08-15 10:24:41 +0430

    edit invoicepayment controller

--- Controller/InvoicePaymentController.cs
+++ Controller/InvoicePaymentController.cs
@@ -2,6 +2,7 @@
 using BookReader.Data.Models;
 using BookReader.Utillities;
 using BookReader.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -14,6 +15,7 @@ namespace BookReader.Controller
 {
     [Route("api/[controller]/[action]/{id?}")]
     [ApiController]
+    [Authorize]
     public class InvoicePaymentController : ControllerBase
     {
         private readonly IUnitOfWork _db;
@@ -35,60 +37,85 @@ namespace BookReader.Controller
 
                 }
                 ).
-                PaginateObjects(page,pageSize).ToListAsync();
+                PaginateObjects(page, pageSize).ToListAsync();
             return Ok(list);
         }
         [HttpGet]
-        public async Task<IActionResult> FindById(int id)
+        public async Task<IActionResult> GetAll(int invoiceId, int page = 1, int pageSize = 10)
         {
-            var invoicePayment = await _db.InvoicePayments.Find(id);
-            if (invoicePayment == null)
-            {
-                return NotFound();
-            }
+            var invoiceList = await _db.InvoicePayments.GetAll()
+                .Where(n => n.InvoiceId == invoiceId)
+                .Select(s => new InvoicePaymentVm
+                {
+                    Id = s.Id,
+                    InvoiceId = s.Invoice.Id,
+                    CreationDate = s.CreationDate,
+                    PayAmount = s.PayAmount,
+                    TransactionId = s.TransactionId
 
-            return Ok(invoicePayment);
+                })
+                .PaginateObjects(page, pageSize)
+                .ToListAsync();
+            return Ok(invoiceList);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Create([FromBody] InvoicePayment invoicePayment)
+        public async Task<IActionResult> Create([FromBody] InvoicePaymentVm invoicePaymentVm)
         {
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
-            invoicePayment.InvoiceId = invoicePayment.Invoice.Id;
-            invoicePayment.CreationDate = DateTime.Now;
-            var result = await _db.InvoicePayments.CreateAsync(invoicePayment);
-            result.Id = invoicePayment.Id;
-            result.Extra = invoicePayment;
-            return Ok(result);
-        }
-        [HttpPut]
-        public async Task<IActionResult> Edit([FromBody] InvoicePayment invoicePayment)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-            var result = await _db.InvoicePayments.EditAsync(invoicePayment);
-            result.Id = invoicePayment.Id;
-            result.Extra = invoicePayment;
-            return Ok(result);
-        }
 
-        [HttpDelete]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var invoicePament = _db.InvoicePayments.Find(id);
-            if(invoicePament == null)
+            InvoicePayment invoicePayment = new InvoicePayment()
             {
-                return NotFound();
-            }
-            var result = await _db.InvoicePayments.DeleteAsync(id);
-            result.Id = id;
-            result.Extra = invoicePament;
+                CreationDate = DateTime.Now,
+                Id = invoicePaymentVm.Id,
+                TransactionId = invoicePaymentVm.TransactionId,
+                InvoiceId = invoicePaymentVm.InvoiceId,
+                PayAmount = invoicePaymentVm.PayAmount,
+
+            };
+
+            var result = await _db.InvoicePayments.CreateAsync(invoicePayment);
+
+            result.Id = invoicePaymentVm.Id;
+            result.Extra = invoicePaymentVm;
+
             return Ok(result);
         }
+        //[HttpPut]
+        //public async Task<IActionResult> Edit([FromBody] InvoicePaymentVm invoicePaymentVm)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
+
+        //    var oldInvoicePayment = await _db.InvoicePayments.Find(invoicePaymentVm.Id);
+        //    oldInvoicePayment.Id = invoicePaymentVm.Id;
+        //    oldInvoicePayment.InvoiceId = invoicePaymentVm.InvoiceId;
+        //    oldInvoicePayment.PayAmount = invoicePaymentVm.PayAmount;
+        //    oldInvoicePayment.TransactionId = invoicePaymentVm.TransactionId;
+
+        //    var result = await _db.InvoicePayments.EditAsync(oldInvoicePayment);
+        //    result.Id = invoicePaymentVm.Id;
+        //    result.Extra = invoicePaymentVm;
+        //    return Ok(result);
+        //}
+
+        //[HttpDelete]
+        //public async Task<IActionResult> Delete(int id)
+        //{
+        //    var invoicePament = _db.InvoicePayments.Find(id);
+        //    if (invoicePament == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var result = await _db.InvoicePayments.DeleteAsync(id);
+        //    result.Id = id;
+        //    result.Extra = invoicePament;
+        //    return Ok(result);
+        //}
     }
 }
